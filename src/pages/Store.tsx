a5ec35d48455ab983e838354afa24172cf02a4bf
Hovhannes Petrosyan <hovopetrosyan2000@gmail.com>
2023-01-23 16:44:19 +0400

    Added Store

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import storeItems from "../data/items.json"
+import StoreItem from '../components/StoreItem'
+import { Row,Col } from 'react-bootstrap'
 
 const Store = () => {
   return (
-    <div>Store</div>
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map(item => (
+          <Col key={item.id} >
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
